06dbe00b8778da7fa66ff0f24a6021b26363ea6d
Kishan Dobariya <kishan.dobariya@bacancytechnology.com>
2020-01-30 14:29:23 +0530

    Set callback url in env.

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,8 +5,7 @@ class Auth extends Component {
     return (
       <div className="AuthWrapper">
         <button type="button" className="btn btn-primary">
-          {/* <a className="AuthLink" href="https://github.com/login/oauth/authorize?client_id=32643f5c08582c341db3&redirect_uri=https://git-manager.herokuapp.com/&scope=user:email,public_repo,repo"> */}
-          <a className="AuthLink" href="https://github.com/login/oauth/authorize?client_id=32643f5c08582c341db3&redirect_uri=http://localhost:3000/&scope=user:email,public_repo,repo">
+          <a className="AuthLink" href={`https://github.com/login/oauth/authorize?client_id=32643f5c08582c341db3&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=user:email,public_repo,repo`}>
             Login with githhub
           </a>
         </button>
